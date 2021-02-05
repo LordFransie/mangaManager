505d270cf8ec8bf9a5e844dd9b45b9ddde09cefc
Frans Alkemade <lordfransie@gmail.com>
2021-02-05 10:27:07 -0500

    Added cheap line buffering

--- manga.py
+++ manga.py
@@ -106,4 +106,4 @@ class Manga:
 
     def download(self):
         print("Downloading "+self.name)
-        os.system('gallery-dl '+self.url)
+        os.system('gallery-dl ' + self.url + '| grep ✔ --line-buffered |')
